acf08bd9a4c7dcf10ea77a714ce5de7f356506fd
Will Alread <will.alread@gmail.com>
2023-03-14 08:23:14 -0400

    removed inter font from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
       backgroundImage: {
         'gradient-background': "url('../images/background.png')",
       },
